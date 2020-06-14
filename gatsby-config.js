331506590e4157a01ccedef571835e48db481c2b
shahriarsohan <sohanmock@gmail.com>
2020-06-14 06:20:48 +0600

    api url

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,10 +28,10 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
         //   contentTypes : `jobs`, `projects`, `blogs`,
-        contentTypes: [`jobs`],
+        contentTypes: [`jobs`, `projects`],
         // singleTypes: [`about`],
       },
     },
