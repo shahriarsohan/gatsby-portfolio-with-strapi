768eeca7a22db4e0ee1d95efe051d1d751dca4e3
shahriarsohan <sohanmock@gmail.com>
2020-06-18 21:30:59 +0600

    strap url modified

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: `https://strapi-cms-007.herokuapp.com`,
         queryLimit: 1000, // Default to 100
         //   contentTypes : `jobs`, `projects`, `blogs`,
         contentTypes: [`jobs`, `projects`],
